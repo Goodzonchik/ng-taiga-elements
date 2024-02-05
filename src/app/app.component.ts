60b5e31f421ae7363bbeaab75b20662af8b332c0
Sergey Glazyrin <goodzonchik@rambler.ru>
2024-02-05 09:02:09 +0500

    update deps and add demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import { TuiRootModule } from '@taiga-ui/core';
+
 @Component({
-  selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
+  imports: [TuiRootModule]
 })
 export class AppComponent {
   title = 'ng-taiga-elements';
